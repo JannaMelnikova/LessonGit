8a3eef47628f7b627cf453f78532d632b2d3a2d5
JannaMelnikova <jannet87@list.ru>
2025-08-14 13:42:11 +0300

    delete printHello "Hi" and LocalDate in merthod printTime

--- src/main/java/org/example/lessongit/Service.java
+++ src/main/java/org/example/lessongit/Service.java
@@ -1,16 +1,17 @@
 package org.example.lessongit;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Service {
 
     public void printHello(){
-        System.out.println("Hi");
         System.out.println("Hi Master");
         System.out.println("Hi User ");
     }
 
     public void  printTime(){
-        System.out.println(LocalDateTime.now());
+        System.out.println(LocalDate.now());
     }
+
 }

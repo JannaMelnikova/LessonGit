9137185f65c07c7316e8ac3394e5e86f750ae388
JannaMelnikova <jannet87@list.ru>
2025-08-14 13:36:27 +0300

    add method printTime for print current data and time

--- src/main/java/org/example/lessongit/Service.java
+++ src/main/java/org/example/lessongit/Service.java
@@ -1,10 +1,16 @@
 package org.example.lessongit;
 
+import java.time.LocalDateTime;
+
 public class Service {
 
     public void printHello(){
         System.out.println("Hi");
         System.out.println("Hi Master");
         System.out.println("Hi User ");
+    }
+
+    public void  printTime(){
+        System.out.println(LocalDateTime.now().toString());
     }
 }
